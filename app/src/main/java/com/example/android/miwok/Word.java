c5ca89d4a033e7350db3b87006b7e201ab07a9da
ashea2120 <ashea2120@yahoo.com>
2019-05-09 12:04:37 -0400

    added second constructor to Word class to include the option to have an image. Adjusted the phrases, family, colors, and numbers classes to use the new constructor as necessary

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,35 +1,31 @@
 package com.example.android.miwok;
 
 public class Word {
-    /** Default translation for the word */
+    //Resources for the translations and the image
     private String mDefaultTranslation;
-
-    /** Miwok translation for the word */
     private String mMiwokTranslation;
+    private int mImageResourceId;
 
-    /**
-     * Create a new Word object.
-     *
-     * @param defaultTranslation is the word in a language that the user is already familiar with
-     *                           (such as English)
-     * @param miwokTranslation is the word in the Miwok language
-     */
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
     }
 
-    /**
-     * Get the default translation of the word.
-     */
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
+    }
+
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
-    /**
-     * Get the Miwok translation of the word.
-     */
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
     }
 }
