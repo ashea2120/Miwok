bd2953de40e1e4fad6938e03974ee27ca0712bb7
ashea2120 <ashea2120@yahoo.com>
2019-05-09 11:31:41 -0400

    removed unused layout files and made numbers, phrases, colors, and family all reference one central layout file

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -11,7 +11,7 @@ public class FamilyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_family);
+        setContentView(R.layout.activity_numbers);
 
         //List of words
         ArrayList<Word> words = new ArrayList<Word>();
