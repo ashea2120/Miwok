31aa9bd82a465d305de31e6784726a2e583c1901
ashea2120 <ashea2120@yahoo.com>
2019-05-09 09:24:34 -0400

    Added a word class to get the translations and provide a better OOP stance with the project

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -23,23 +23,17 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView numbers = (TextView) findViewById(R.id.numbers);
-    TextView family = (TextView) findViewById(R.id.family);
-    TextView colors = (TextView) findViewById(R.id.colors);
-    TextView phrases = (TextView) findViewById(R.id.phrases);
-
-
-
-    //family.setOnClickListener(new View.OnClickListener() {}
-    //colors.setOnClickListener(new View.OnClickListener() {}
-    //phrases.OnClickListener(new View.OnClickListener() {}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        TextView numbers = findViewById(R.id.numbers);
+        TextView family = findViewById(R.id.family);
+        TextView colors = findViewById(R.id.colors);
+        TextView phrases = findViewById(R.id.phrases);
 
         // Set a click listener on that View
         numbers.setOnClickListener(new View.OnClickListener() {
